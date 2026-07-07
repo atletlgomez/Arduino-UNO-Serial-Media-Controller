840583d8d7e4b364301c857236982a0781ef1d2a
gomez <atletlgomez1@gmail.com>
2026-07-07 15:45:21 -0700

    play pause button code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//time
+
+unsigned long currentTime = 0;
+unsigned long prevTime = 0;
+unsigned long bounceDelay = 150;
+
+
+int currentButtonState;
+int prevButtonState;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  DDRD &= ~(1<< 2);
+  PORTD |= (1 << 2);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  currentTime = millis();
+
+//saving state 
+  if ((PIND & (1 << 2)) == 0) {
+    currentButtonState = LOW;
+  } else {
+    currentButtonState = HIGH;
+  }
+
+
+
+  if ((currentButtonState == LOW) && (prevButtonState == HIGH) && ((currentTime - prevTime) >= bounceDelay)) {
+    Serial.println("PLAY_PAUSE");
+    prevTime = currentTime;
+  }
+
+  prevButtonState = currentButtonState;
+
+
 }
---
+++ src/media_controller.py
@@ -0,0 +1,24 @@
+import serial
+import time
+import subprocess
+
+arduino_port = "/dev/ttyACM0"
+print("Connected. Waiting for Arduino messages...")
+baud_rate = 9600
+
+arduino = serial.Serial(arduino_port, baud_rate)
+
+time.sleep(2)
+
+while True:
+    message = arduino.readline()
+    message = message.decode("utf-8")
+    message = message.strip()
+
+
+    if message == "PLAY_PAUSE":
+        subprocess.run(["playerctl", "-p", "spotify", "play-pause"])
+
+    if message:
+        print("Received command:", message)
+
